c3df0c38bbdf5d6f00da3b72c010d234e523affe
trishayvonne <faztrisha12@gmail.com>
2023-09-23 11:06:41 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('test','MainController::test');
+$routes->post('save','MainController::save');
+$routes->get('/Update/(:any)','MainController::Update/$1');
+$routes->get('/Delete/(:any)','MainController::Delete/$1');
